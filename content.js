f8b939100a8a35fc56f66ae5b895e9c3a065c5db
Rasha <66318514+rashafathima@users.noreply.github.com>
2023-06-25 16:53:45 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,11 +1,10 @@
-// Listen for a message from the popup
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action === "extractTermsAndConditions") {
     try {
       // Extract terms and conditions content
       const termsAndConditionsContent = extractTermsAndConditions();
-
-      // Send the extracted content to the popup
+      
       sendResponse({ content: termsAndConditionsContent });
     } catch (error) {
       console.error('Error extracting terms and conditions content:', error);
@@ -15,10 +14,6 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 });
 
 function extractTermsAndConditions() {
-  // TODO: Implement the logic to extract terms and conditions content from the webpage.
-  // You can use DOM manipulation or other techniques to target and extract the content.
-
-  // Example: Extracting the text content of all paragraphs on the page
   const paragraphs = Array.from(document.getElementsByTagName("p"));
   const content = paragraphs.map(p => p.textContent).join("\n");
 
